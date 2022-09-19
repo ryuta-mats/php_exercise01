9e2a2b8096147f34f8fcee0bf0c17cb439031eaa
ryuta-mats <matsu.r.1987@gmail.com>
2022-09-19 17:54:16 +0900

    制御構文2の課題修正

--- 05/02_if2.php
+++ 05/02_if2.php
@@ -5,7 +5,7 @@ $fund_raising = 101;
 
 // ここに処理を記述
 echo "あなたの所持金は{$poket_money}円です｡" . PHP_EOL;
-if ($poket_money > $fund_raising) {
+if ($poket_money >= $fund_raising) {
     do {
         $poket_money -= $fund_raising;
         echo "{$fund_raising}円募金しました。" . PHP_EOL;
